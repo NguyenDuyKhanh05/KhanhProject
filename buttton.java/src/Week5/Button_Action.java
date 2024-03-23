f8687b48718504c15538294441f1836621571442
NguyenDuyKhanh05 <23010335@st.phenikaa-uni.edu.vn>
2024-03-23 23:22:12 +0700

    01

---
+++ buttton.java/src/Week5/Button_Action.java
@@ -0,0 +1,17 @@
+package Week5;
+
+import javafx.event.ActionEvent;
+public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == searchButton) {
+       
+    } else if (e.getSource() == createButton) {
+      
+    } else if (e.getSource() == deleteButton) {
+       
+    }
+public class Button_Action {
+    @Override
+
+}
+    
+}
