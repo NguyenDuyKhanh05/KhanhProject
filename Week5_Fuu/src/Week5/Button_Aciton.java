2927d23e70ad0364473769b3a004b5be53a7716e
NguyenDuyKhanh05 <23010335@st.phenikaa-uni.edu.vn>
2024-03-24 00:11:07 +0700

    01

---
+++ Week5_Fuu/src/Week5/Button_Aciton.java
@@ -0,0 +1,18 @@
+package Week5;
+
+import javafx.event.ActionEvent;
+public void actionPerformed(ActionEvent e) {
+public class Button_Aciton {
+
+
+    if (e.getSource() == searchButton) {
+   
+    } else if (e.getSource() == createButton) {
+   
+    } else if (e.getSource() == deleteButton) {
+  
+} 
+    }
+}
+    
+}
